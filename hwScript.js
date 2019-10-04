70b6e7b859bd12046da0e2b1de082fa29ffd53de
David Baas <Dcbaas@users.noreply.github.com>
2019-10-03 21:33:30 -0400

    Did inital fetch example

--- hwScript.js
+++ hwScript.js
@@ -7,23 +7,52 @@ el.addEventListener("click", ev => {
 });
 
 // Delete the old list. Rather than delete the li elements, I delete the whole list.
-// Then I restart from scratch. 
+// Then I restart from scratch.
 const deleteOldList = () => {
-  let victim = document.querySelector('ol');
-  if(victim !== null) {
+  let victim = document.querySelector("ol");
+  if (victim !== null) {
     victim.parentNode.removeChild(victim);
   }
 };
 
 // Populates the list with text. Rather than putting all of the functionality in
-// Action listener, I send the action listener to a function. 
-const populateList = amount => {
-  let content = document.getElementById('jsContent');
+// Action listener, I send the action listener to a function.
+const populateList = async amount => {
+  let content = document.getElementById("jsContent");
   let orderedList = document.createElement("ol");
-  for (let i = 0; i < amount; i++) {
-    let listItem = document.createElement('li');
-    listItem.appendChild(document.createTextNode(`Item #${i + 1}`));
+
+  let olNode = await fetch(`https://randomuser.me/api?results=${amount}`)
+    .then(response => response.json())
+    .then(data => {
+      let content = document.getElementById("jsContent");
+      let orderedList = document.createElement("ol");
+      data.results.forEach(element => {
+        let listItem = document.createElement("li");
+        listItem.appendChild(
+          document.createTextNode(`${element.name.first} ${element.name.last}`)
+        );
+        orderedList.appendChild(listItem);
+      });
+    
+      return orderedList;
+    });
+  content.appendChild(olNode);
+};
+
+// REDUNDENT 
+const convertResponseToJson = response => response.json();
+
+// REDUNDENT 
+const showData = data => {
+  let content = document.getElementById("jsContent");
+  let orderedList = document.createElement("ol");
+  data.results.forEach(element => {
+    let listItem = document.createElement("li");
+    listItem.appendChild(
+      document.createTextNode(`${element.name.first} ${element.name.last}`)
+    );
     orderedList.appendChild(listItem);
-  }
-  content.appendChild(orderedList);
+  });
+
+  return orderedList;
 };
