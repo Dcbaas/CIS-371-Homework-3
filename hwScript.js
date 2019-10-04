09a455b581a41944141d24913c0a3896a7527112
David Baas <Dcbaas@users.noreply.github.com>
2019-10-03 20:28:27 -0400

    Finished step 13

--- hwScript.js
+++ hwScript.js
@@ -2,15 +2,27 @@ const el = document.getElementById("goBtn");
 el.addEventListener("click", ev => {
   const inp = document.getElementById("numInput");
   //   alert("Your number is " + inp.value);
+  deleteOldList();
   populateList(inp.value);
 });
 
+// Delete the old list. Rather than delete the li elements, I delete the whole list.
+// Then I restart from scratch. 
+const deleteOldList = () => {
+  let victim = document.querySelector('ol');
+  if(victim !== null) {
+    victim.parentNode.removeChild(victim);
+  }
+};
+
+// Populates the list with text. Rather than putting all of the functionality in
+// Action listener, I send the action listener to a function. 
 const populateList = amount => {
   let content = document.getElementById('content');
   let orderedList = document.createElement("ol");
   for (let i = 0; i < amount; i++) {
     let listItem = document.createElement('li');
-    listItem.appendChild(document.createTextNode(`Item #${i}`));
+    listItem.appendChild(document.createTextNode(`Item #${i + 1}`));
     orderedList.appendChild(listItem);
   }
   content.appendChild(orderedList);
